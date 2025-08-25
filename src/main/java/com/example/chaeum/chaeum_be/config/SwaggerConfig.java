dd75e63ead365cd4c7bec32287ed01a5e53a66b9
egaeuni <gaeun3619@gmail.com>
2025-08-25 17:43:42 +0900

    :bug: Fix: swaggerConfig 수정

--- src/main/java/com/example/chaeum/chaeum_be/config/SwaggerConfig.java
+++ src/main/java/com/example/chaeum/chaeum_be/config/SwaggerConfig.java
@@ -2,8 +2,11 @@ package com.example.chaeum.chaeum_be.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -12,8 +15,14 @@ public class SwaggerConfig {
         Info info = new Info()
                 .title("2025-likelion-chaeum API") //이름
                 .description("채움, 비어있는 공간에 가치를 더하다."); //설명
+
+        Server httpsServer = new Server().url("https://egaeuni.shop");
+
+
         return new OpenAPI()
-                .info(info);
+                .info(info)
+                .servers(List.of(httpsServer));
+
     }
 
 }
