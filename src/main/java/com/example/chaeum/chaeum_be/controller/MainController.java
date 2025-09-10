f7b2203095fa05034fcf24a6b538d70585041e99
noeyadd <wkdekdus6778@naver.com>
2025-09-10 20:45:33 +0900

    ♻️ [REFACT] JWT 기반 코드 리팩토링

--- src/main/java/com/example/chaeum/chaeum_be/controller/MainController.java
+++ src/main/java/com/example/chaeum/chaeum_be/controller/MainController.java
@@ -1,19 +1,16 @@
 package com.example.chaeum.chaeum_be.controller;
 
-import com.example.chaeum.chaeum_be.code.ErrorCode;
 import com.example.chaeum.chaeum_be.code.ResponseCode;
-import com.example.chaeum.chaeum_be.dto.response.ErrorResponseDTO;
 import com.example.chaeum.chaeum_be.dto.response.ResponseDTO;
 import com.example.chaeum.chaeum_be.entity.User;
-import com.example.chaeum.chaeum_be.exception.GlobalException;
-import com.example.chaeum.chaeum_be.repository.UserRepository;
 import com.example.chaeum.chaeum_be.service.MainService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,24 +18,10 @@ import org.springframework.web.bind.annotation.*;
 public class MainController {
 
     private final MainService mainService;
-    private final UserRepository userRepository;
-
-    private User getLoggedInUser() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String email = authentication.getName();
-        return userRepository.findUserByEmail(email)
-                .orElseThrow(() -> new GlobalException(ErrorCode.USER_NOT_FOUND));
-    }
 
     @Operation(summary = "메인(사용자 추천 + 최신 등록 집)")
     @GetMapping
-    public ResponseEntity<?> getMain() {
-        User loginUser = getLoggedInUser();
-        if (loginUser == null) {
-            return ResponseEntity.status(ErrorCode.UNAUTHORIZED_UESR.getStatus().value())
-                    .body(new ErrorResponseDTO(ErrorCode.UNAUTHORIZED_UESR, null));
-        }
-
+    public ResponseEntity<?> getMain(@AuthenticationPrincipal User loginUser) {
         var dto = mainService.loadMain(loginUser);
         return ResponseEntity
                 .status(ResponseCode.SUCCESS_GET_MAIN.getStatus().value())
